c6bf8bd337d773e716d37870fd27af32392d01eb
hronikiz <dolbaniob@gmail.com>
2025-05-23 13:02:20 +0300

    Update main3.cpp

--- graf/main3.cpp
+++ graf/main3.cpp
@@ -14,6 +14,8 @@ int main() {
 
     vector<bool> used(n, false);
     vector<int> minEdge(n, INF);
+    vector<int> parent(n, -1);
+
     minEdge[0] = 0;
     int total = 0;
 
@@ -22,14 +24,22 @@ int main() {
         for (int j = 0; j < n; ++j)
             if (!used[j] && (v == -1 || minEdge[j] < minEdge[v]))
                 v = j;
+
         used[v] = true;
         total += minEdge[v];
 
         for (int u = 0; u < n; ++u)
-            if (w[v][u] && w[v][u] < minEdge[u])
+            if (w[v][u] && !used[u] && w[v][u] < minEdge[u]) {
                 minEdge[u] = w[v][u];
+                parent[u] = v;
+            }
     }
 
-    cout << \"Вес остовного дерева: \" << total << endl;
+    cout << "Вес остовного дерева: " << total << endl;
+    cout << "Ребра в минимальном остовном дереве:\n";
+    for (int i = 1; i < n; ++i)
+        if (parent[i] != -1)
+            cout << parent[i] + 1 << " - " << i + 1 << " (вес " << w[i][parent[i]] << ")\n";
+
     return 0;
 }
